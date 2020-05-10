9c4b42956ff1464d648c08df2ddf6ac04b42b00c
Prabhas <prabhas@Prabhass-MacBook-Pro.local>
2020-05-10 14:05:01 +0530

    first version

---
+++ src/main/java/com/alycin/CredentialsApiApplication.java
@@ -0,0 +1,13 @@
+package com.alycin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentialsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alycin/CredentialsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alycin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
